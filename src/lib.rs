bd3c566e5fdd9ab46dc323939ff952d0df708071
Masterchef365 <duncan.freeman1@gmail.com>
2025-07-23 00:43:31 -0700

    Factor out into lib some

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,130 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use egui::{epaint, Color32, Rgba};
+use euc::{Buffer2d, CullMode, Pipeline, Sampler, Target, Texture, TriangleList};
+
+#[derive(Clone, Copy, Debug)]
+pub struct EguiVertexData {
+    pub uv: egui::Pos2,
+    pub color: egui::Rgba,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl std::ops::Mul<f32> for EguiVertexData {
+    type Output = Self;
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self {
+            uv: self.uv.to_vec2().mul(rhs).to_pos2(),
+            color: self.color.mul(rhs),
+        }
     }
 }
+
+impl std::ops::Add<Self> for EguiVertexData {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            uv: self.uv + rhs.uv.to_vec2(),
+            color: self.color + rhs.color,
+        }
+    }
+}
+
+impl From<epaint::Vertex> for EguiVertexData {
+    fn from(value: epaint::Vertex) -> Self {
+        EguiVertexData {
+            uv: value.uv,
+            color: value.color.into(),
+        }
+    }
+}
+
+pub struct EguiMeshEucPipeline<'r, S> {
+    pub sampler: S,
+    pub vertices: &'r [epaint::Vertex],
+}
+
+impl<'r, S> Pipeline<'r> for EguiMeshEucPipeline<'r, S>
+where
+    S: Sampler<2, Index = f32, Sample = egui::Rgba>,
+{
+    type Vertex = u32;
+    type VertexData = EguiVertexData;
+    type Primitives = TriangleList;
+    type Pixel = u32;
+    type Fragment = Rgba;
+
+    #[inline(always)]
+    fn vertex(&self, idx: &Self::Vertex) -> ([f32; 4], Self::VertexData) {
+        let vertex = self.vertices[*idx as usize];
+        let xyzw = [vertex.pos.x / 100.0, vertex.pos.y / 100.0, 0.0, 1.0];
+        (xyzw, vertex.into())
+    }
+
+    #[inline(always)]
+    fn fragment(&self, color: Self::VertexData) -> Self::Fragment {
+        color.color
+    }
+
+    fn blend(&self, _: Self::Pixel, color: Self::Fragment) -> Self::Pixel {
+        u32::from_le_bytes(color.to_srgba_unmultiplied())
+    }
+
+    fn rasterizer_config(
+            &self,
+        ) -> CullMode {
+        CullMode::None
+    }
+}
+
+pub struct Scissor<T> {
+    inner: T,
+    x: usize,
+    y: usize,
+    width: usize,
+    height: usize,
+}
+
+impl<T> Scissor<T> {
+    pub fn new(inner: T, x: usize, y: usize, width: usize, height: usize) -> Self {
+        Self {
+            inner,
+            x,
+            y,
+            width,
+            height,
+        }
+    }
+
+    fn bounds_check(&self, x: usize, y: usize) -> bool {
+        x >= self.x && y >= self.y && x < self.x + self.width && y < self.y + self.height
+    }
+}
+
+impl<T, const N: usize> Texture<N> for Scissor<T>
+where
+    T: Texture<N>,
+{
+    type Index = T::Index;
+    type Texel = T::Texel;
+
+    fn size(&self) -> [Self::Index; N] {
+        self.inner.size()
+    }
+
+    fn read(&self, index: [Self::Index; N]) -> Self::Texel {
+        self.inner.read(index)
+    }
+}
+
+impl<T: Target> Target for Scissor<T> {
+    unsafe fn read_exclusive_unchecked(&self, x: usize, y: usize) -> Self::Texel {
+        unsafe { self.inner.read_exclusive_unchecked(x, y) }
+    }
+
+    unsafe fn write_exclusive_unchecked(&self, x: usize, y: usize, texel: Self::Texel) {
+        if self.bounds_check(x, y) {
+            unsafe {
+                self.inner.write_exclusive_unchecked(x, y, texel);
+            }
+        }
+    }
+}
+
